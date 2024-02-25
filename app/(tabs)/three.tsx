ddee4df4d10cf84a06ac76329b816b1b669122b2
ascaryaaa <allaamascarya@gmail.com>
2024-02-25 22:08:54 +0700

    implmented flex

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -3,9 +3,33 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 1, backgroundColor: 'red'}}>
+        <Text style={styles.title}>red</Text>
+      </View>
+      <View style={{flex: 2, backgroundColor: 'darkorange'}}>
+        <Text style={styles.title}>darkorange</Text>
+      </View>
+      <View style={{flex: 3, backgroundColor: 'green'}}>
+        <Text style={styles.title}>green</Text>
+      </View>
+    </View>
+  );
+};
+
 export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
+      <Flex/>
       <Text style={styles.title}>Tab Three</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/three.tsx" />

be6cec94990f2ee35cc04f6595d15d03fbb169f0
ascaryaaa <allaamascarya@gmail.com>
2024-02-26 09:54:34 +0700

    implemented scroll view

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,17 +1,41 @@
-import { StyleSheet, Image } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet, Image, Button,Alert, ScrollView } from 'react-native';
 import { Text, View} from '@/components/Themed';
+import React, {Component} from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <Image source={require('../../assets/images/favicon.png')} />
-      <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
-       style={{width: 400, height: 400}} />
-    </View>
-  );
+export default class TabTwoScreen extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.container}>
+            <Text style={styles.title}>Tab Two</Text>
+            <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
+              style={{width: 400, height: 400}} />
+            <Image source={require('../../assets/images/favicon.png')} />
+            <View style={styles.buttonContainer}>
+              <Button onPress={this._onPressButton} title="Press Me" />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={this._onPressButton}
+                title="Press Me"
+                color="#841584"
+              />
+            </View>
+            <View style={styles.alternativeLayoutButtonContainer}>
+              <Button onPress={this._onPressButton} title="This looks great!" />
+              <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -28,5 +52,17 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
 });
